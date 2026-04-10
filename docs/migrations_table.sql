a1d4c32ac986eb548b691e9a4f3f08e299316771
Leonardo Gian Pottmayer <leonardo.pottmayer@gmail.com>
2026-04-10 08:09:31 -0300

    Added checksum column to migrations table.

--- docs/migrations_table.sql
+++ docs/migrations_table.sql
@@ -3,5 +3,6 @@ CREATE TABLE
         migration_id VARCHAR(255) PRIMARY KEY,
         status CHAR(1) NOT NULL,
         updated_at TIMESTAMP,
-        application_batch_id INTEGER
+        application_batch_id INTEGER,
+        checksum VARCHAR(64)
     );
